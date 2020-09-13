9484d84972de54361946835b1e6272c76f5031d6
YaelJannai <57910052+YaelJannai@users.noreply.github.com>
2020-09-13 22:29:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ var io = require('socket.io');
  
 var app = express();
 
-app.use(express.static('./public'));
-//Specifying the public folder of the server to make the html accesible using the static middleware
-
 app.get('/', (req, res) => {
   console.log(req)
   res.send('Welcome to Comeet!\nmeet your study buddies, and maybe future friends :)\n')
 })
+
+app.use(express.static('./public'));
+//Specifying the public folder of the server to make the html accesible using the static middleware
+
 
 var server = http.createServer(app).listen(8080);
 //Server listens on the port 8080
@@ -29,26 +30,30 @@ io.sockets.on("connection",function(socket){
       /*sending data to the client , this triggers a message event at the client side */
     console.log('Socket.io Connection with the client established');
     socket.on("message",function(data){
-        /*This event is triggered at the server side when client sends the data using socket.send() method */
-        data = JSON.parse(data);
- 
-        console.log(data);
+    /*This event is triggered at the server side when client sends the data using socket.send() method */
+      data = JSON.parse(data);
 
+      console.log(data);
+
+      // if (!(data.email.endsWith('@post.bgu.ac.il'))){
+      //   /*Printing the data */
+      //   var err_to_client = {
+      //     data:'ERROR! your email is not valid'
+      //   }
+      //   //socket.send(JSON.stringify(err_to_client));
+      // }
+      //else{
+      if (data.email.endsWith('@post.bgu.ac.il')){
         /*Printing the data */
         var ack_to_client = {
           data:'\nServer Received the message.'
         }
+        // var destination = '/questions.html';
+        // io.sockets.emit('redirect', destination);
         socket.send(JSON.stringify(ack_to_client));
-        /*Sending the Acknowledgement back to the client , this will trigger "message" event on the clients side*/
+      /*Sending the Acknowledgement back to the client , this will trigger "message" event on the clients side*/
 
-        if (!(data.email.endsWith('@post.bgu.ac.il'))){
-          /*Printing the data */
-          var err_to_client = {
-            data:'ERROR! your email is not valid'
-          }
-          socket.send(JSON.stringify(err_to_client));
-        }
-        
-    });
+      }
+  });
  
 });
