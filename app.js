c5b77b03ac4b6f17e768f31c96214fd5a6450d2b
YaelJannai <57910052+YaelJannai@users.noreply.github.com>
2020-09-14 14:34:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-var express=require("express"); 
-var bodyParser=require("body-parser"); 
+const express = require("express"); 
+const bodyParser = require("body-parser"); 
 
-// const mongoose = require('mongoose'); 
-// mongoose.connect('mongodb://localhost:27017/gfg', {useNewUrlParser: true}); 
-// var db=mongoose.connection; 
-// db.on('error', console.log.bind(console, "connection error")); 
-// db.once('open', function(callback){ 
-	// console.log("connection succeeded"); 
-// }) 
-
-var app=express();
+var app = express();
 
 
 app.use(bodyParser.json()); 
@@ -18,34 +10,63 @@ app.use(bodyParser.urlencoded({
 	extended: true
 })); 
 
-app.post('/sign_up', function(req,res){ 
-	var name = req.body.name; 
-	var email =req.body.email; 
-	var pass = req.body.password; 
-	var phone =req.body.phone; 
+// configuration for json file with user input
+var jsonfile = require('jsonfile');    
+var file = './userdata.json'
 
-	var data = { 
-		"name": name, 
-		"email":email, 
-		"password":pass, 
-		"phone":phone 
-	} 
-// db.collection('details').insertOne(data,function(err, collection){ 
-// 		if (err) throw err; 
-// 		console.log("Record inserted Successfully"); 
-			
-// 	}); 
-		
-	return res.redirect('signup_success.html'); 
-}) 
-
-
+// what happens when the web page is opened
 app.get('/',function(req,res){ 
 res.set({ 
 	'Access-control-Allow-Origin': '*'
 	}); 
 return res.redirect('index.html');
-}).listen(3000);
+})
 
+// after the user signed in, save data
+app.post('/sign_up', function(req,res){ 
+	var name = req.body.name; 
+	var email =req.body.email; 
 
-console.log("server listening at port 3000"); 
+	var data = { 
+		"name": name, 
+		"email":email
+	} 
+
+	return res.redirect('/questions.html'); 
+}) ;
+
+// after the user answered questions, save answers
+app.post('/details', function(req,res){ 
+	var name = req.body.name; 
+	var degree_1 = req.body.degree_1; 
+	var degree_2 = (req.body.degree_2 == "") ? null :  req.body.degree_2;
+	var year = req.body.year;
+	var courses = req.body.courses;
+	var hobbies = req.body.hobbies;
+	var any = req.body.any;
+
+	// create student object
+	var data = { 
+		"name": name, 
+		"degree_1":degree_1,
+		"degree_2":degree_2,
+		"year":year,
+		"courses":courses,
+		"hobbies":hobbies,
+		"any": any
+	} 
+	// add to JSON file 
+	jsonfile.writeFileSync(file, data, {flag: 'a'});
+	return res.end();
+
+});
+
+// the app will listen to port 3000
+app.listen(3000, err => {
+	if (err) {
+	  console.log("there was a problem", err);
+	  return;
+	}
+	console.log("listening on port 3000");
+  });
+
