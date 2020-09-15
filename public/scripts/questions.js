8b834cf2f31f6d45b0e7cbdbe8f4152e39c29958
Ariel Jannai <arieljannai@gmail.com>
2020-09-15 11:58:30 +0300

    some more design changes

--- public/scripts/questions.js
+++ public/scripts/questions.js
@@ -4,5 +4,5 @@ function onloadfn() {
     console.log('questions onload');
     var data = JSON.parse(httpGet('/sign_up'));
     document.getElementById('name').value = data.name;
-    document.querySelector('#user-details > h2').innerHTML = 'היי ' + data.name.split(' ')[0] + '!<BR>הנה כמה שאלות כדי שנוכל להכיר טוב יותר :)';
+    document.querySelector('#user-details > h2').innerHTML = 'היי ' + data.name.split(' ')[0] + '!';
 };
