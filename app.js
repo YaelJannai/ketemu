69f58c2db9f502c53dc264260a6d65083b7b4afb
YaelJannai <57910052+YaelJannai@users.noreply.github.com>
2020-09-15 10:55:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 22060843f40e81c510ee9729795370a6eff5acd4
 let {PythonShell} = require('python-shell')
 
 
@@ -11,41 +7,16 @@ const fs = require('fs')
 const file = 'userdata.json'
 // configuration for json file with user input
 const jsonfile = require('jsonfile');   
-<<<<<<< HEAD
-=======
-let {PythonShell} = require('python-shell');
-const express = require("express");
-const bodyParser = require("body-parser");
-const http = require("http");
->>>>>>> e70af87b12339d025244e0435bcd544d11be84a0
-=======
->>>>>>> 22060843f40e81c510ee9729795370a6eff5acd4
 
 var app = express();
 var isTime = false;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 22060843f40e81c510ee9729795370a6eff5acd4
 app.use(bodyParser.json()); 
 app.use(express.static('public')); 
 app.use(bodyParser.urlencoded({ 
 	extended: true
 })); 
  
-<<<<<<< HEAD
-=======
-app.use(bodyParser.json());
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// configuration for json file with user input
-var jsonfile = require("jsonfile");
-var file = "./userdata.json";
->>>>>>> e70af87b12339d025244e0435bcd544d11be84a0
-=======
->>>>>>> 22060843f40e81c510ee9729795370a6eff5acd4
 
 // what happens when the web page is opened
 app.get('/',function(req,res){ 
@@ -54,7 +25,6 @@ res.set({
 	}); 
 return res.redirect('index.html');
 })
-
 
 
 // after the user signed in, save data
@@ -72,116 +42,6 @@ app.post('/sign_up', function(req,res){
 
 });
 
-<<<<<<< HEAD
-
-// after the user answered questions, save answers
-<<<<<<< HEAD
-app.post('/details', function(req,res){ 
-	if (!isTime){
-		var name = req.body.name; 
-		var degree_1 = req.body.degree_1; 
-		var degree_2 = (req.body.degree_2 == "") ? null :  req.body.degree_2;
-		var year = req.body.year;
-		var courses = (req.body.courses == "") ? [] : req.body.courses;
-		var hobbies = (req.body.hobbies == "") ? [] : req.body.hobbies;
-		var any = req.body.any;
-		var id = makeid();
-
-		fields = [degree_1, degree_2]
-	
-		// create student object
-		var data = { 
-			"id": id,
-			"given-name": name, 
-			"studies": {"fields":fields, "year":year},
-			"courses":courses,
-			"hobbies":hobbies,
-			"extra-info": any
-		} 
-
-		// add to JSON file 
-		jsonfile.writeFileSync(file, data, {flag: 'a'});
-		
-		//call to python function
-		InitUsersPython();
-		getMeetingsPython()
-
-		// wait for the json file from getMeetingsPython to be created
-		// then use the file to create meetings
-		const checkTime = 1000;
-		function check() {
-			setTimeout(() => {
-				fs.readFile('groups.json', 'utf8', function(err, data) {
-					if (err) {
-						// got error reading the file, call check() again
-						check();
-					} else {
-						// we have the file contents here, so do something with it
-						var meetingsData = JSON.parse(fs.readFileSync('groups.json', 'utf-8'));
-						//console.log(meetingsData);
-					}
-				});
-			}, checkTime)
-		}
-
-		check();
-
-		// var meetingsData = jsonfile.readFileSync('groups.json', 'utf-8');
-		//console.log(meetingsData);
-
-		//var meetingsData = JSON.parse(fs.readFileSync('groups.json', 'utf-8'));
-		
-		// console.log(res);
-		//recieve url of the meeting
-		//send user to meeting
-
-		res.redirect('/meeting.html');
-		return res.end('bye!');
-	}
-=======
-app.post("/details", function (req, res) {
-    if (!isTime) {
-        var name = req.body.name;
-        var degree_1 = req.body.degree_1;
-        var degree_2 = req.body.degree_2 == "" ? null : req.body.degree_2;
-        var year = req.body.year;
-        var courses = req.body.courses == "" ? [] : req.body.courses;
-        var hobbies = req.body.hobbies == "" ? [] : req.body.hobbies;
-        var any = req.body.any;
-        var id = makeid();
-
-        fields = [degree_1, degree_2];
-
-        // create student object
-        var data = {
-            user_id: id,
-            name: name,
-            degree_1: degree_1,
-            degree_2: degree_2,
-            year: year,
-            courses: courses,
-            hobbies: hobbies,
-            any: any,
-        };
-
-        cachedUser["details"] = data;
-        // add to JSON file
-        jsonfile.writeFileSync(file, data, { flag: "a" });
-        var jsonObj = JSON.stringify(data);
-        //call to python function
-        InitUsersPython();
-        var res = getMeetingsPython();
-        console.log(res);
-        //recieve url of the meeting
-        //send user to meeting
-
-        res.redirect("/meeting.html");
-        return res.end("bye!");
-    }
->>>>>>> e70af87b12339d025244e0435bcd544d11be84a0
-});
-=======
->>>>>>> 22060843f40e81c510ee9729795370a6eff5acd4
 
 // after the user answered questions, save answers
 app.post('/details', function(req,res){ 
@@ -212,7 +72,7 @@ app.post('/details', function(req,res){
 		
 		//call to python function
 		InitUsersPython();
-		getMeetingsPython()
+		getMeetingsPython();
 
 		// wait for the json file from getMeetingsPython to be created
 		// then use the file to create meetings
@@ -292,10 +152,6 @@ function createMeetings(){
 	}, 60 * 1000 * 20); //call the function every 20 minutes	
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 22060843f40e81c510ee9729795370a6eff5acd4
 
 /**************************************************************PYTHON******************************************************************/
 
@@ -312,7 +168,6 @@ function InitUsersPython(){
 	PythonShell.run('script.py', options, function (err) {
 		if (err) throw err;
 	});
-<<<<<<< HEAD
 }
 
 
@@ -328,51 +183,4 @@ function getMeetingsPython(){
 	PythonShell.run('script.py', options, function (err, results) {
 		if (err) throw err;
 	});
-=======
-function InitUsersPython() {
-    let options = {
-        mode: "json",
-        pythonPath: "python",
-        pythonOptions: ["-u"], // get print results in real-time
-        scriptPath: ".",
-        args: ["init", "/userdata.json"],
-    };
-    PythonShell.run("script.py", options, function (err, results) {
-        if (err) throw err;
-        // results is an array consisting of messages collected during execution
-        console.log("results: %j", results);
-    });
-}
-
-function getMeetingsPython() {
-    let options = {
-        mode: "json",
-        pythonPath: "python",
-        pythonOptions: ["-u"], // get print results in real-time
-        scriptPath: ".",
-        args: ["get_groups"],
-    };
-    PythonShell.run("script.py", options, function (err, results) {
-        if (err) throw err;
-        return results;
-        //json object - array of objects with meetingID and userID of users for that meeting
-    });
->>>>>>> e70af87b12339d025244e0435bcd544d11be84a0
-=======
-}
-
-
-function getMeetingsPython(){
-	let options = {
-		mode: 'json',
-		pythonPath: 'python',
-		pythonOptions: ['-u'], // get print results in real-time
-		scriptPath: '.',
-		args: ['get_groups']
-	  };
-	   
-	PythonShell.run('script.py', options, function (err, results) {
-		if (err) throw err;
-	});
->>>>>>> 22060843f40e81c510ee9729795370a6eff5acd4
 }
